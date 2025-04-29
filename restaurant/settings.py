65ea094f01a515eb9ca1b21d0625a705ab21feff
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-04-29 17:04:32 +0100

    Add Secret Key

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g4-!2tl1n4h2i=4%7ef#5$5e7fmf4ah&px#smbo@&vridi3f_&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
