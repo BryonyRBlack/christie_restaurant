9fc1b60fa24f2b45716382e0f3bd218c33a0dba7
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-04-30 15:14:04 +0100

    Fix CSRF typo

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "http://*.codeinstitute-ide.net/",
-    "https://christie-restaurant-fa4bd564fbe8.herokuapp.com/"
+    "https://*.herokuapp.com/"
 ]
 
 
