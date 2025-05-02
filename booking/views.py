57bfab74bc8b99ae2bf815b3849eae5152f19329
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-02 17:35:31 +0100

    Add Booking html, and crispy forms for request

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Booking
+from .forms import BookingForm
 
 # Create your views here.
 def my_booking(request):
-    return HttpResponse("Hello, World!")
+    booking_form = BookingForm()
+
+    return render(
+        request,
+        "booking/booking.html",
+        {
+            "Booking":Booking,
+            "booking_form":booking_form,
+        }
+    )
