380cf1d196723fa6a9b80a83a9c3e6e7b8a8a32d
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-02 17:41:25 +0100

    Add Contact form for html

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
 def contact_us(request):
-    return HttpResponse("Hello, World!")
+    contact_form = ContactForm()
+    return render(
+        request, "contact/contact.html",
+        {
+            "contact_form":contact_form
+        }
+    )
