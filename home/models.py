3e2c7144331a2908d34f2ad5bbd89de06f9d3ec3
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-07 15:58:21 +0100

    Add styling to index page

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Home(models.Model):
     title = models.CharField(max_length=200)
+    side_image = CloudinaryField('image', default='placeholder')
     content = models.TextField(blank=True, null=True)
 
     def __str__(self):
