5a484ac1693219d9a8ab295b33f6c58e2abdaa3f
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-08 15:02:47 +0100

    Add confirmation alert for contact page

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Contact
 from .forms import ContactForm
@@ -6,6 +7,19 @@ from .forms import ContactForm
 # Create your views here.
 def contact_us(request):
     contact_form = ContactForm()
+
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact = contact_form.save(commit=False)
+            contact.author = request.user
+            contact.post = contact
+            contact.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'You message has been received, we will be in contact shortly'
+            )
+
     return render(
         request, "contact/contact.html",
         {
