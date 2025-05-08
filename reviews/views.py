b6a19f6839e04db48ef4d0c7ff694cc6c42568ae
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-08 14:54:29 +0100

    Add code for reviews correctly submitted

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from django.http import HttpResponse
+from django.contrib import messages
 from .models import Review
 from . forms import ReviewForms
 
@@ -28,6 +27,18 @@ from . forms import ReviewForms
 def my_reviews(request):
     reviews = Review.objects.all().order_by('-created_at')
     review_form = ReviewForms
+
+    if request.method == "POST":
+        review_form = ReviewForms(data=request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.author = request.user
+            review.post = reviews
+            review.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Review submitted succesfully'
+            )
 
     return render(
         request,
