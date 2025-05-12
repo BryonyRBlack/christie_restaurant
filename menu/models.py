31c48ff60fbbb03442da199cf90f54d64decda04
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-12 12:23:28 +0100

    Add code so menu items are visable on page

--- menu/models.py
+++ menu/models.py
@@ -6,19 +6,35 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
     def __str__(self):
         return self.name
     
+
 class Food(models.Model):
     category = models.ForeignKey(
         Category,
-        related_query_name='food_items',
+        related_name='food_items',
         on_delete=models.CASCADE
     )
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     image = CloudinaryField('image', default='placeholder')
     available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+        ]
 
     def __str__(self):
         return self.name
--- menu/views.py
+++ menu/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 from .models import Category, Food
 # Create your views here.
+
+#class FoodList(generic.ListView):
+    #queryset = Food.objects.all()
+    #template_name = "menu.html"
 
 def food_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    food = Food.objects.filter(available=True)
+    products = Food.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        food = food.filter(category=category)
+        products = products.filter(category=category)
     return render(
         request,
-        'menu/menu.html'
+        'menu/menu.html',
+        {
+            'category': category,
+            'categories': categories,
+            'products': products
+        }
     )
