b2315e30a9e84a1766b2334cabc9f4d12d517f8c
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-15 12:08:23 +0100

    Add summernote to news articles

--- news/admin.py
+++ news/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ['title', 'slug', 'published']
     list_filter = ['published', 'title']
     search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('body')
