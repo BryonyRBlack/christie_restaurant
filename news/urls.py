7951492f59411407450dde072c6da774cb10911c
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-15 18:32:38 +0100

    Fix typos for broken links

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='news'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
