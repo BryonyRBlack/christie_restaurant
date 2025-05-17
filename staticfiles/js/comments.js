18b140edd2826e839f8eb9c7e02d6afc8055274f
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-17 17:54:56 +0100

    Fix static files

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -1,14 +1,31 @@
+// This pulls the required items needed for succesful editing and deleting from the html
+
 const editButtons = document.getElementsByClassName('edit-btn');
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("commentButton");
 
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("del-btn");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+
+//This shows that if the button is clicked, than the comment needs to be edited
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("comment_id");
+        let commentId = e.target.getAttribute("data-comment_id");
         let commentContent = document.getElementById(`comment${commentId}`).innerText;
         commentText.value = commentContent;
         submitButton.innerText = "Update";
         commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
+
+//This allows for if the button is clicked, the pop up confirming deletion comes up
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("data-comment_id");
+        deleteConfirm.href = `delete_comment/${commentId}`;
+        deleteModal.show();
     });
 }
