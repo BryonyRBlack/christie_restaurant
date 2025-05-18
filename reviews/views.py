1e445398d4db0cf8f69b87f7195d72af0d4dd759
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2025-05-18 16:54:43 +0100

    Remove defective code

--- reviews/views.py
+++ reviews/views.py
@@ -1,28 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.contrib import messages
 from .models import Review
 from . forms import ReviewForms
-
-# Create your views here.
-
-#class ReviewList(generic.ListView):
-    #queryset = Review.objects.filter(status=1)
-    #template_name = "reviews/reviews.html"
-
-
-#def my_reviews(request):
-    #review = Review.objects.all().order_by('-created_at')
-    #review_form = ReviewForms
-
-
-    #return render(
-        #request,
-        #"reviews/reviews.html",
-        #{
-            #"review_form": review_form,
-            #"review": review,
-        #}   
-    #)
 
 def my_reviews(request):
     reviews = Review.objects.all().order_by('-created_at')
@@ -32,7 +11,7 @@ def my_reviews(request):
         review_form = ReviewForms(data=request.POST)
         if review_form.is_valid():
             review = review_form.save(commit=False)
-            review.author = request.user
+            review.user = request.user
             review.post = reviews
             review.save()
             messages.add_message(
